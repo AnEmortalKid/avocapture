f7c416ca20e96a3cc32b8d3ebb9071d6b7890c44
Jan Monterrubio <janmonterrubio@gmail.com>
2022-05-28 11:03:50 -0500

    form binding

--- src/main/views/entry/entryEditor.js
+++ src/main/views/entry/entryEditor.js
@@ -3,10 +3,27 @@ console.log('entryEditor');
 const { ipcRenderer } = require("electron");
 
 
+const replayPrefix = "replay.prefix";
+const replayTitle = "replay.title";
+
+function bindToForm(data) {
+  const { prefix } = data;
+
+  const replayPrefixInput = document.getElementById(replayPrefix);
+  replayPrefixInput.value = prefix;
+}
+
+function bindFromForm() {
+  return {
+    prefix: document.getElementById(replayPrefix).value,
+    title: document.getElementById(replayTitle).value
+  }
+}
+
 function bindButtons() {
   var applyBtn = document.getElementById("entry-apply-btn");
   applyBtn.addEventListener("click", (event) => {
-    ipcRenderer.send("Entry.Dialog.Apply", {});
+    ipcRenderer.send("Entry.Dialog.Apply", bindFromForm());
   });
 
   var cancelBtn = document.getElementById("entry-cancel-btn");
@@ -18,5 +35,6 @@ function bindButtons() {
 ipcRenderer.on("Entry.Dialog.Initialize", (event, data) => {
   console.log("Received Entry.Initialize");
   console.log(JSON.stringify(data));
+  bindToForm(data);
   bindButtons();
 });
