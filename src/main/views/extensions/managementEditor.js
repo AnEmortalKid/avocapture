3145786ecb41c189a49d478abc824cc6eb1fa5cc
AnEmortalKid <janmonterrubio@gmail.com>
2022-06-25 13:56:51 -0500

    theming

--- src/main/views/extensions/managementEditor.js
+++ src/main/views/extensions/managementEditor.js
@@ -5,3 +5,20 @@ console.log('Manager loaded');
 ipcRenderer.on('ExtensionManagement.Initialize', (event, data) => {
   console.log(data);
 });
+
+
+/* 
+<li class="w3-bar w3-disabled">
+  <button class="w3-button w3-round w3-theme-action w3-right">Uninstall <i class="fa fa-trash"></i></button>
+  <div class="w3-bar-item">
+    <span class="w3-large">Search on Hotkey</span><br>
+  </div>
+  <div class="w3-bar-item">
+    <p>An extension for avocapture that searches the filesystem when a hotkey is pressed.</p>
+  </div>
+</li> 
+*/
+function createListItem(extensionName) {
+
+
+}
