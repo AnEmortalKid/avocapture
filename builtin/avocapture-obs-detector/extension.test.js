6f6076076267a96d47cfeaa5edeadf4c4376c933
AnEmortalKid <janmonterrubio@gmail.com>
2022-07-13 08:50:49 -0500

    retry test

--- builtin/avocapture-obs-detector/extension.test.js
+++ builtin/avocapture-obs-detector/extension.test.js
@@ -1,9 +1,30 @@
+jest.useFakeTimers('modern');
+jest.spyOn(global, 'setInterval');
+jest.spyOn(global, 'clearInterval');
+
 var mockLogger;
 
 const ObsWebSocket = require('obs-websocket-js');
 jest.mock('obs-websocket-js');
 
 const ObsEventDetector = require('./extension')
+
+function createSuccessPromise() {
+  return new Promise((resolve, reject) => {
+    resolve({ msg: "hello" });
+  })
+}
+
+function createFailurePromise() {
+  return new Promise((resolve, reject) => {
+    reject(
+      {
+        error: {
+          code: 100, message: "not available"
+        }
+      });
+  })
+}
 
 describe("extension", () => {
 
@@ -27,6 +48,46 @@ describe("extension", () => {
     // };
   });
 
+  describe('connection handling', () => {
+
+    // todo advance a few times
+    test("attempts to reconnect on an interval", async () => {
+      let mockConnection = jest.fn().mockImplementationOnce(
+        createFailurePromise)
+        .mockImplementationOnce(
+          createFailurePromise
+        ).mockImplementationOnce(
+          createSuccessPromise
+        )
+
+      ObsWebSocket.default.mockImplementation(() => {
+        return {
+          connect: mockConnection
+        }
+      });
+
+      const od = new ObsEventDetector({ logger: mockLogger });
+      od.initialize({
+        serverPort: 4444,
+        reconnectIntervalSeconds: 1
+      });
+
+      expect(setInterval).toHaveBeenCalledTimes(1);
+
+      // first error
+      jest.advanceTimersByTime(1000);
+      await Promise.resolve();
+      // 2nd error
+      jest.advanceTimersByTime(1000);
+      await Promise.resolve();
+      // success connect
+      jest.advanceTimersByTime(1000);
+      await Promise.resolve();
+
+      expect(clearInterval).toHaveBeenCalledTimes(1);
+    });
+  });
+
   // connect fail -> keep retrying
   // connect success -> sets up listeners
 
@@ -42,5 +103,5 @@ describe("extension", () => {
       serverPort: 4444
     });
 
-  }, 50000);
+  }, 100000);
 })
