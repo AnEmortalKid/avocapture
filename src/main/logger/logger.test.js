df8db0026cd0869d5f602569f797dff7acaefcae
AnEmortalKid <janmonterrubio@gmail.com>
2022-07-15 19:48:29 -0500

    still broke

--- src/main/logger/logger.test.js
+++ src/main/logger/logger.test.js
@@ -1,30 +1,9 @@
-// This works with require
-// jest.mock('electron', () => {
-//   return {
-//     app: {
-//       getPath: jest.fn()
-//     },
-//   };
-// });
-
-// did not work
-// jest.mock('electron', () => {
-//   // Require the original module to not be mocked...
-//   // const originalModule = jest.requireActual('electron');
-
-//   return {
-//     __esModule: true, // Use it when dealing with esModules
-//     //...originalModule,
-//     app: { getPath: jest.fn().mockReturnValue('testUserData') },
-//   };
-// });
-
-// this is so dumb
-jest.mock('electron', () => ({
-  __esModule: true, // this property makes it work
-  getPath: jest.fn(),
-}));
-import { app } from 'electron'
+import { app } from 'electron';
+jest.mock('electron', () => {
+  return {
+    getPath: jest.fn().mockReturnValue("testUserData")
+  };
+});
 
 
 jest.mock('fs');
