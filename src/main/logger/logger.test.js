4aaf1b000b92ce380fe6443fcc2c81a9e45840f5
AnEmortalKid <janmonterrubio@gmail.com>
2022-07-15 23:52:03 -0500

    logger package

--- src/main/logger/logger.test.js
+++ src/main/logger/logger.test.js
@@ -10,14 +10,46 @@ const electronLog = require('electron-log');
 
 import Logger from "./logger";
 
-describe('setup', () => {
-  test('works', () => {
+describe('Logger', () => {
+
+  let mockLogger;
+
+  beforeEach(() => {
+    mockLogger = {
+      info: jest.fn()
+    }
+    electronLog.scope.mockImplementation(() => mockLogger);
+  });
+
+  test('creates scoped logger', () => {
     electronLog.scope = jest.fn();
 
     const loggy = Logger.create('testFile');
 
-    expect(electronLog.scope).toHaveBeenCalled();
+    expect(electronLog.scope).toHaveBeenCalledWith('testFile');
+  });
 
+  test('logEvent formats message correctly', () => {
+    const loggy = Logger.create('testFile');
 
+    loggy.logEvent('someEvent', 'received { foo }')
+
+    expect(mockLogger.info).toHaveBeenCalledWith('[@ someEvent] received { foo }');
+  });
+
+  test('logMethod formats message correctly', () => {
+    const loggy = Logger.create('testFile');
+
+    loggy.logMethod('myMethod', 'received { foo }')
+
+    expect(mockLogger.info).toHaveBeenCalledWith('[myMethod] received { foo }');
+  });
+
+  test('log formats message correctly', () => {
+    const loggy = Logger.create('testFile');
+
+    loggy.log('a random message')
+
+    expect(mockLogger.info).toHaveBeenCalledWith('a random message');
   });
 });
