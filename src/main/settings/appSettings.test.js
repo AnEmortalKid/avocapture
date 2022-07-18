c76a94db68887f9b874828a15f79bc8e8a9afa4c
Jan Monterrubio <janmonterrubio@gmail.com>
2022-07-18 13:00:44 -0500

    apsetting test

--- src/main/settings/appSettings.test.js
+++ src/main/settings/appSettings.test.js
@@ -1,19 +1,21 @@
 const mockSet = jest.fn()
-
+const mockDelete = jest.fn()
+const mockGet = jest.fn()
 
 import { Store } from 'electron-store';
 
+//do it like this since it is a default export
+//and we can't reference a var
 jest.mock("electron-store", () => {
   return jest.fn().mockImplementation(() => {
     return {
-      set: (k, v) => mockSet(k, v)
+      set: (k, v) => mockSet(k, v),
+      delete: (k) => mockDelete(k),
+      get: (k) => mockGet(k)
     };
   })
 })
 
-// jest.mock('path/to/module', () => ({
-//   X: jest.fn()
-// }));
 
 import { AppSettings } from "./appSettings";
 
@@ -29,5 +31,29 @@ describe("AppSettings", () => {
     appSettings.save("someKey", 'valueObj');
 
     expect(mockSet).toHaveBeenCalledWith("someKey", "valueObj");
+  });
+
+  test("save deletes key when not given data", () => {
+    const appSettings = new AppSettings();
+
+    appSettings.save("someKey");
+
+    expect(mockDelete).toHaveBeenCalledWith("someKey");
+  });
+
+  test("get retrieves value", () => {
+    const appSettings = new AppSettings();
+
+    appSettings.get("someKey");
+
+    expect(mockGet).toHaveBeenCalledWith("someKey")
+  });
+
+  test("clear deletes key", () => {
+    const appSettings = new AppSettings();
+
+    appSettings.clear("someKey");
+
+    expect(mockDelete).toHaveBeenCalledWith("someKey")
   });
 });
