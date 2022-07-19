ac975d555bbee6d0da9a6b5900ef62512f3b0b7b
AnEmortalKid <janmonterrubio@gmail.com>
2022-07-19 08:46:36 -0500

    setup mocks for extInst

--- src/main/extensions/installer/extensionInstaller.test.js
+++ src/main/extensions/installer/extensionInstaller.test.js
@@ -4,13 +4,57 @@ jest.mock('./copyUtils');
 jest.mock('fs');
 const fs = require('fs');
 
-import { installExtension } from './extensionInstaller'
+jest.mock('child_process')
+
+jest.mock('electron', () => {
+  return {
+    app: {
+      getPath: jest.fn().mockReturnValue('fakeUserData')
+    }
+  }
+});
+
+import { copyDirectory, copyAssets } from './copyUtils'
+
+
+import installExtension from './extensionInstaller'
+
+const execSync = require('child_process').execSync;
 
 describe("extensionInstaller", () => {
 
-  test("does it", () => {
+  // const env = process.env
 
-    installExtension('somePath');
+  // beforeEach(() => {
+  //   jest.resetModules()
+  //   process.env = { ...env }
+  // })
+
+  // afterEach(() => {
+  //   process.env = env
+  // })
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("installs net new extension", () => {
+    fs.readFileSync.mockReturnValue(JSON.stringify({
+      name: "test-extension",
+      version: "0.1.0",
+    }));
+
+    installExtension('test-extension');
+
+    expect(execSync).toHaveBeenCalled();
+
+  });
+
+  test("installs new version of an extension", () => {
+
+  });
+
+  test("does not install older extension", () => {
 
   });
 });
