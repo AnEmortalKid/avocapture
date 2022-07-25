4ca047ce1e9a8c9b5bf28786a1924e2279c48443
AnEmortalKid <janmonterrubio@gmail.com>
2022-07-24 22:02:31 -0500

    prefix install

--- test_extensions.sh
+++ test_extensions.sh
@@ -1,3 +1,4 @@
+npm install --prefix builtin/avocapture-search-on-hotkey
 npm run test --prefix builtin/avocapture-search-on-hotkey
 npm run test --prefix builtin/avocapture-replay-mover
 npm run test --prefix builtin/avocapture-obs-detector
